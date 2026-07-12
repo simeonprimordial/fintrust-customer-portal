1ea46c4d3202b5b56f1edb874d607945e9b2e795
Simeon Ige Siaka <simeonprimordial@gmail.com>
2026-07-12 17:46:33 +0100

    feat: containerize and deploy Flask app with ALB, ASG, RDS and ECR

--- app/config.py
+++ app/config.py
@@ -1,26 +1,4 @@
 import os
-
-
-# class Config:
-
-#     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
-
-#     DB_USERNAME = os.getenv("DB_USERNAME")
-#     DB_PASSWORD = os.getenv("DB_PASSWORD")
-#     DB_HOST = os.getenv("DB_HOST")
-#     DB_PORT = os.getenv("DB_PORT", "3306")
-#     DB_NAME = os.getenv("DB_NAME")
-
-#     SQLALCHEMY_DATABASE_URI = (
-#         f"mysql+pymysql://"
-#         f"{DB_USERNAME}:{DB_PASSWORD}"
-#         f"@{DB_HOST}:{DB_PORT}/"
-#         f"{DB_NAME}"
-#     )
-
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
